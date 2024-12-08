86353e289ea75d20adac84936b6878fc8a43f13a
restu <restu.pradana@cashbac.com>
2024-12-09 01:07:42 +0700

    minor

--- src/utils/verify_password.rs
+++ src/utils/verify_password.rs
@@ -2,8 +2,8 @@ use argon2::{Argon2, PasswordHash, PasswordVerifier};
 
 pub fn verify_password(hash: &str, password: &str) -> Result<bool, Box<dyn std::error::Error>> {
    let parsed_hash = PasswordHash::new(hash).map_err(|e| {
-      eprintln!("Failed to create params {:?}", e);
-      "failed create params"
+      eprintln!("Failed to hashing password {:?}", e);
+      "failed hashing password"
    })?;
    let argon2 = Argon2::default();
    match argon2.verify_password(password.as_bytes(), &parsed_hash) {
